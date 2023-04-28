95a43266cd8dd62e5e7981a68810f4776d186b83
Anuj <anuj89806@gmail.com>
2023-04-28 14:21:23 +0530

    solved count character in a string problem

---
+++ frequencyCounterPattern.js
@@ -0,0 +1,8 @@
+console.log("------------Frequency Counter Pattern------------");
+
+// ? 1. Write a function called "same", which accepts two arrays. The function should return true if every value in the array has it's corresponding value squared in the second array. The frequency of values must be the same.
+
+// console.log(same([1, 2, 3], [4, 1, 9])); // true
+// console.log(same([1, 2, 3], [1, 9])); // false
+// console.log(same([1, 2, 3], [4, 4, 1])); // false
+// console.log(same([1, 2, 3, 5, 2], [4, 4, 1, 9, 25])); // true
