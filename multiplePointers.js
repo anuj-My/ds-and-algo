08ff266c7631f81fbb57f5ffe4af716b50160df1
Anuj <anuj89806@gmail.com>
2023-05-02 13:31:56 +0530

    solved countUniqueValues problem using multiple pointers

--- multiplePointers.js
+++ multiplePointers.js
@@ -36,8 +36,30 @@ const sumZero2 = (arr) => {
   }
 };
 
-console.log(sumZero2([-3, -2, -1, 0, 1, 2, 3]));
-console.log(sumZero2([-2, 0, 1, 3]));
-console.log(sumZero2([1, 2, 3]));
-console.log(sumZero2([-4, -3, -2, 1, 2, 3, 4, 5, 6]));
-console.log(sumZero2([-4, -3, -2, -1, 0, 1, 2, 5]));
+// console.log(sumZero2([-3, -2, -1, 0, 1, 2, 3]));
+// console.log(sumZero2([-2, 0, 1, 3]));
+// console.log(sumZero2([1, 2, 3]));
+// console.log(sumZero2([-4, -3, -2, 1, 2, 3, 4, 5, 6]));
+// console.log(sumZero2([-4, -3, -2, -1, 0, 1, 2, 5]));
+
+// ? 2. Implement a function called countUniqueValues, which accepts a sorted array, and counts the unique values in the array. There can be negative numbers in the array, but it will always be sorted.
+
+function countUniqueValues(arr) {
+  let i = 0;
+  if (arr.length === 0) return i;
+
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+
+  return i + 1;
+}
+
+console.log(countUniqueValues([1, 1, 1, 1, 2]));
+console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]));
+console.log(countUniqueValues([-2, -1, -1, 0, 1]));
+console.log(countUniqueValues([1, 1, 1, 1]));
+console.log(countUniqueValues([]));
