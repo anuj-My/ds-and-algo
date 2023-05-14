1d2eb82595c39270cd7adf4baa32185b1e10cda4
Anuj <anuj89806@gmail.com>
2023-05-14 12:23:52 +0530

    solved averagePair using multiple Pointers pattern

--- multiplePointers.js
+++ multiplePointers.js
@@ -89,6 +89,32 @@ const areThereDuplicates = (...arr) => {
   return false;
 };
 
-console.log(areThereDuplicates(1, 2, 3));
-console.log(areThereDuplicates(1, 2, 2));
-console.log(areThereDuplicates("a", "a", "b", "c"));
+// console.log(areThereDuplicates(1, 2, 3));
+// console.log(areThereDuplicates(1, 2, 2));
+// console.log(areThereDuplicates("a", "a", "b", "c"));
+
+// ? 4. Write a function called averagePair. Given a sorted array of integers and a target average, determine if there is a pair of values in the array where the average of the pair equals the target average. There may be more than one pair that matches the average target.
+
+function averagePair(arr, target) {
+  let first = 0;
+  let last = arr.length - 1;
+
+  while (first < last) {
+    let avg = arr[first] + arr[last] / 2;
+
+    if (avg === target) {
+      return true;
+    } else if (avg < target) {
+      first++;
+    } else if (avg > target) {
+      last--;
+    }
+  }
+
+  return false;
+}
+
+console.log(averagePair([1, 2, 3], 2.5));
+console.log(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8));
+console.log(averagePair([-1, 0, 3, 4, 5, 6], 4.1));
+console.log(averagePair([], 4));
